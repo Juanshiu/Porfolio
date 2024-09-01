8d7b8a279a1d7b563b45409c277cc69628e5c96e
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-08-31 19:39:45 -0500

    Intento de internacionalización v1

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const ui = {
+    en: {
+        "site.title": "Astro Blog",
+        "newpage.title": "New page title",
+        "newpage.description": "New page description"
+    },
+    fr: {
+        "site.title": "Astro Blog",
+        "newpage.title": "Nouvelle page",
+        "newpage2.title": "Nouvelle page description"
+    },
+    es: {
+        "site.title": "Astro Blog",
+        "newpage.title": "Nueva pagina",
+        "newpage2.title": "Nueva pagina description"
+    }
+}
